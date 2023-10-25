e3617db742632182f99050d7f52aa6b75a28ddb7
MBareiro <58647378+MBareiro@users.noreply.github.com>
2023-10-25 17:15:27 -0300

    remove unused imports

--- app.py
+++ app.py
@@ -3,10 +3,7 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-from flask_login import UserMixin
-from flask_mail import Mail, Message
-import os
+from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
 login_manager = LoginManager(app)
