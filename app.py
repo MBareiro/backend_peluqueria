65232bf0795dcb34f121089ad4968bc5c3d5bbc5
MBareiro <58647378+MBareiro@users.noreply.github.com>
2023-10-25 15:46:21 -0300

    prueba railway

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from flask_login import UserMixin
 from flask_mail import Mail, Message
-
+import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
 login_manager = LoginManager(app)
@@ -15,7 +15,13 @@ CORS(app)
 
 # Configurar la base de datos
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mbdev:Pruebas132@mbdev.mysql.pythonanywhere-services.com/mbdev$default'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/proyecto'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/proyecto?useSSL=false&serverTimezone=UTC'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:wcbnjd9ycbpjukmvx2wwofh5vhfdgb36@monorail.proxy.rlwy.net:25231/railway?useSSL=false&serverTimezone=UTC'
+# Configura la base de datos utilizando las variables de entorno
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{os.environ['MYSQLUSER']}:{os.environ['MYSQLPASSWORD']}@{os.environ['MYSQLHOST']}/{os.environ['MYSQLDATABASE']}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -33,20 +39,20 @@ def hello():
     return jsonify(message='¡Hola mundo!')
 
 #Comentar en pythonanywhere
-from controllers.usuario_controller import *
+""" from controllers.usuario_controller import *
 from controllers.horario_controller import *
-from controllers.appointment import *
+from controllers.appointment import * """
 from controllers.reset_password import *
 
 # Descomentar en pythonanywhere 
-"""
+
 from controllers import usuario_controller
 app.route('/usuarios', methods=['GET'])(usuario_controller.get_usuarios)
 app.route('/usuarios/<id>', methods=['GET'])(usuario_controller.get_usuario)
 app.route('/usuarios/<id>', methods=['DELETE'])(usuario_controller.delete_usuario)
 app.route('/usuarios', methods=['POST'])(usuario_controller.create_usuario)
 app.route('/usuarios/<id>', methods=['PUT'])(usuario_controller.update_usuario)
-"""
+
 
 # programa principal, comentar en pythonanywhere
 if __name__ == '__main__':
